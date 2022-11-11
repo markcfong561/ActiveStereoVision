a2f8cfbcf9e6e964d0e459cc5e5a6afc27c0b547
markcfong561 <markcfong561@gmail.com>
2022-11-10 23:25:13 -0500

    Found checkboard (Just their rows, not particular points)

--- ShowCorners.py
+++ ShowCorners.py
@@ -9,28 +9,61 @@ def distance(point1, point2):
     distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
     return distance
 
-def orderPoints(points: np.ndarray, yThreshold: int):
+def orderPoints(points: np.ndarray, threshold: int, byColumn: bool):
     rows = {}
     currentRow = []
     for point in points:
         x, y = point.ravel()
-        currentYCord = y
+        if byColumn:
+            interestCord = x
+        else:
+            interestCord = y
         # Go through rows created, see if new point can go inside
-        for yCord in rows.keys():
-            if abs(y - yCord) < yThreshold:
-                currentRow = rows.get(yCord)
-                currentYCord = yCord
+        for position in rows.keys():
+            if abs(interestCord - position) < threshold:
+                currentRow = rows.get(position)
+                interestCord = position
                 break
         currentRow.append(point)
-        rows[currentYCord] = currentRow
+        rows[interestCord] = currentRow
         currentRow = []
+    # Sort points left/right or up/down
     for row in rows:
-        rows[row] = sorted(rows[row], key=lambda x: x[0][0])
+        rows[row] = sorted(rows[row], key=lambda x: x[0][1 * byColumn])
     return rows
 
-def findCheckboard(cornerList: List):
-    
-    pass
+
+def findCheckboard(rowList: dict[int, List], colList: dict[int, List], shape: tuple[int, int]):
+    w, h = shape
+    eligableRows = []
+    eligableCols = []
+    for row in rowList:
+        if len(rowList[row]) >= w:
+            eligableRows.append(rowList[row])
+    if len(eligableRows) >= h:
+        for col in colList:
+            if len(colList[col]) >= h:
+                eligableCols.append(colList[col])
+        if len(eligableCols) >= w:
+            firstInRows = [tuple(point.tolist()[0]) for point in eligableRows[:][:][0]]
+            firstInCols = [tuple(point.tolist()[0]) for point in eligableCols[:][:][0]]
+            topLeftList = list(set(firstInRows) & set(firstInCols))
+            if len(topLeftList) > 0:
+                topLeft = topLeftList[0]
+                firstRow = list(rowList.keys())[(np.abs(np.subtract(list(rowList.keys()), topLeft[0]))).argmin()]
+                checkboardList = []
+                i = 0
+                foundRow = False
+                for row in rowList.keys():
+                    if row == firstRow:
+                        foundRow = True
+                    if foundRow:
+                        checkboardList.append(rowList[row])
+                        i += 1
+                    if i == h:
+                        break
+                return checkboardList
+    return None
 
 def prettyPrintOrdered(orderedPoints):
     for element in orderedPoints:
@@ -41,39 +74,39 @@ def prettyPrintOrdered(orderedPoints):
 def main():
     # Live Video
     video = cv2.VideoCapture(0)
-
+# 
     keyPress = None
-
+# 
     while keyPress != ord('q'):
         ret, image = video.read()
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        blankImg = np.zeros(image.shape)
         if ret:
             corners = cv2.goodFeaturesToTrack(gray,40,0.01,10)
             cornerPoints = np.int0(corners)
 
             for cornerPoint in cornerPoints:
                 y, x = cornerPoint.ravel()
-                cv2.circle(image, (y, x), 4, color=(0,0,255), thickness=10)
+                cv2.circle(image, (y, x), 4, color=(0,0,255), thickness=2)
 
-            orderedPoints = orderPoints(cornerPoints, 20)
-            orderedPoints = collections.OrderedDict(sorted(orderedPoints.items()))
+            rowOrderedPoints = orderPoints(cornerPoints, 20, False)
+            rowOrderedPoints = collections.OrderedDict(sorted(rowOrderedPoints.items()))
+            colOrderedPoints = orderPoints(cornerPoints, 20, True)
+            colOrderedPoints = collections.OrderedDict(sorted(colOrderedPoints.items()))
+
+            checkboardCorners = findCheckboard(rowOrderedPoints, colOrderedPoints, (6, 4))
+            # print(topLeft)
+            if checkboardCorners is not None:
+                for row in checkboardCorners:
+                    for point in row:
+                        x, y = point.ravel()
+                        cv2.circle(image, (x,y), 4, color=(0,255,0), thickness=2)
 
             # prettyPrintOrdered(orderedPoints)
-
-            i = 1
-            for row in orderedPoints:
-                for point in orderedPoints[row]:
-                    y, x = point.ravel()
-                    cv2.putText(image, str(i), (y+10, x+10), cv2.FONT_HERSHEY_PLAIN, 2, color=(0, 0, 255), thickness=1)
-                    i += 1
-            
+            # 
             cv2.imshow("Webcam", image)
             keyPress = cv2.waitKey(5)
         else:
             print("Error opening video feed")
-    while keyPress != ord('q'):
-        keyPress = cv2.waitKey(5)
 
     # Still image for testing
     # image = cv2.imread('Checkerboard.jpg')
@@ -81,20 +114,29 @@ def main():
 
     # corners = cv2.goodFeaturesToTrack(gray,40,0.01,10)
     # cornerPoints = np.int0(corners)
-    # orderedPoints = orderPoints(cornerPoints, 20)
-    # orderedPoints = collections.OrderedDict(sorted(orderedPoints.items()))
-    # prettyPrintOrdered(orderedPoints)
+    # rowOrderedPoints = orderPoints(cornerPoints, 30, False)
+    # rowOrderedPoints = collections.OrderedDict(sorted(rowOrderedPoints.items()))
+    # colOrderedPoints = orderPoints(cornerPoints, 30, True)
+    # colOrderedPoints = collections.OrderedDict(sorted(colOrderedPoints.items()))
+    # # prettyPrintOrdered(orderedPoints)
 
     # for cornerPoint in cornerPoints:
-    #     y, x = cornerPoint.ravel()
-    #     cv2.circle(image, (y,x), 4, color=(0,0,255), thickness=5)
+    #     x, y = cornerPoint.ravel()
+    #     cv2.circle(image, (x,y), 4, color=(0,0,255), thickness=5)
 
     # i = 1
-    # for row in orderedPoints:
-    #     for point in orderedPoints[row]:
-    #         y, x = point.ravel()
-    #         cv2.putText(image, str(i), (y+10, x+10), cv2.FONT_HERSHEY_PLAIN, 2, color=(0, 0, 255), thickness=1)
+    # for row in rowOrderedPoints:
+    #     for point in rowOrderedPoints[row]:
+    #         x, y = point.ravel()
+    #         cv2.circle(image, (x,y), 4, color=(0,0,255), thickness=5)
+    #         cv2.putText(image, str(i), (x+10, y+10), cv2.FONT_HERSHEY_PLAIN, 2, color=(0, 0, 255), thickness=1)
     #         i += 1
+    #     i = 1
+
+    # topLeft = findCheckboard(rowOrderedPoints, colOrderedPoints, (6, 4))
+    # print(topLeft)
+    # x, y = topLeft
+    # cv2.circle(image, (x,y), 4, color=(0,255,0), thickness=5)
 
     # cv2.imshow("Checkboard", image)
 
